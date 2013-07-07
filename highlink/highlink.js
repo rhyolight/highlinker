9b252e25ad15ecc713f02153cf8163d24f2d419f
Matthew Taylor <mtaylor@numenta.com>
2013-07-07 11:35:12 -0700

    Figuring out reference replacements.

--- highlink/highlink.js
+++ highlink/highlink.js
@@ -1,28 +1,72 @@
 var request = require('request'),
-    $ = require('jquery'),
+    jsdom = require('jsdom'),
     highlightStyle = "background-color:yellow";
 
-function updateReferences(body) {
-    var $html = $(body);
+function updateReferences(url, cb) {
+    // $('html').html('');
+    // var html = $(page).find('html').html();
+    // $(html).appendTo('html');
+    // console.log($('html').html());
+    jsdom.env({
+        url: url,
+        scripts: ['http://code.jquery.com/jquery-1.5.min.js'],
+        done: function (err, window) {
+            if (err) throw err;
+            var $ = window.jQuery;
+            var $html = $('html');
+            // jQuery is now loaded on the jsdom window created from 'agent.body'
+            replaceReferentialLinksWithAbsolute($, $html, url);
+            cb('<html>' + $html.html() + '</html>');
+        }
+    });
 }
 
-function replaceReferentialLinksWithAbsolute(html) {
-    return html;
+function replaceReference($el, attr, ref, url) {
+    var baseUrl;
+    
+    if (! ref) return;
+    
+    baseUrl = url.split('/').slice(0, 3).join('/');
+    // If this is an absolute reference to the base URL...
+    if (ref.indexOf('/') == 0) {
+        var newHref = baseUrl + ref;
+        console.log('changing ' + ref + ' to ' + newHref);
+        $el.attr(attr, newHref);
+    }
+}
+
+function replaceReferentialLinksWithAbsolute($, $html, url) {
+    $html.find('link, a, script').each(function() {
+        var $el = $(this);
+        var href = $el.attr('href');
+        var src = $el.attr('src');
+        replaceReference($el, 'href', href, url);
+        replaceReference($el, 'src', src, url);
+    });
 }
 
 function highlightText(body, text) {
-    var re = new RegExp(text, 'g');
-    return body.replace(
-        re, '<span style="' + highlightStyle + '">' + text + '</span>'
-    );
+    var re, 
+        count = 1,
+        out = body;
+    if (text) {
+        re = new RegExp(text, 'g');
+        out = body.replace(
+            re, function(match, p1, p2, p3, offset, string) {
+                return '<span id="highlink-' + (count++) + '" style="' + highlightStyle + '">' + match + '</span>'
+            }
+        );
+    }
+    return out;
 }
 
 function highlink(url, text, callback) {
-    request.get(url, function(err, response) {
-        var absolute = replaceReferentialLinksWithAbsolute(response.body);
-        var highlighted = highlightText(absolute, text);
-        callback(err, highlighted);
-    });
+    // request.get(url, function(err, response) {
+        updateReferences(url, function(body) {
+            var highlighted = highlightText(body, text);
+            callback(null, highlighted);
+        });
+    // });
 }
 
 module.exports = highlink;
